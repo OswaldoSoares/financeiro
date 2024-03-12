923caa62f441340b089af01b8e2be38eae75e9f6
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-12 10:16:00 -0300

    Create class-Models Accounts

--- monetary/models.py
+++ monetary/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Accounts(models.Model):
+    """
+        Model Accounts
+    Attributes:
+        name:
+        description:
+
+    """
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, unique=True)
+    description = models.TextField(max_length=240)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    class Meta:  # pylint: disable=R0903
+        """ Sorted by name field """
+        db_table = "monetary_accounts"
+        ordering = ("name",)
+
+    def save(self, *args, **kwargs):
+        """ Save datas in uppercase """
+
+        self.name = self.name.upper()
+        self.description = self.description.upper()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """ String for representing the ModelName object """
+
+        return str(self.name)
+
+    objects = models.Manager()
