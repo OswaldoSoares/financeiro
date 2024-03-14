1fdc19e8ee108ff9879a17cfc4ca7695ca7b6cd0
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-14 00:12:42 -0300

    Imported urls from website and added path include

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from website import urls as website_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(website_urls)),
 ]
