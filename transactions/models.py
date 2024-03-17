65c6345dd078e9eb8c7d9cec1549a05f2208ac10
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-16 22:43:27 -0300

    Created class-Model Registries

--- transactions/models.py
+++ transactions/models.py
@@ -55,3 +55,46 @@ class Categories(models.Model):
         super().save(*args, **kwargs)
 
     objects = models.Manager()
+
+
+class Registries(models.Model):
+    """
+        Saves registry of incoming and outgoing movements
+    Attributes:
+        favored:
+        obs:
+
+    """
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    companies = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    category_n1 = models.ForeignKey(
+        Categories, related_name="category_n1", on_delete=models.CASCADE
+    )
+    category_n2 = models.ForeignKey(
+        Categories, related_name="category_n2", on_delete=models.CASCADE
+    )
+    category_n3 = models.ForeignKey(
+        Categories, related_name="category_n3", on_delete=models.CASCADE
+    )
+    favored = models.CharField(max_length=20)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    ordering = models.IntegerField(default=0)
+    obs = models.TextField(max_length=120)
+
+    class Meta:  # pylint: disable=R0903
+        """ Sorted by name field """
+        db_table = "transactions_registries"
+        ordering = ["date", "ordering", ]
+
+    def __str__(self):
+        """ String for representing the ModelName object """
+        return str(self.companies)
+
+    def save(self, *args, **kwargs):
+        self.favored = self.favored.upper()
+        self.obs = self.obs.upper()
+
+        super().save(*args, **kwargs)
+
+    objects = models.Manager()
