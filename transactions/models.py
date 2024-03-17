225f1fb05dc82e4b926c7bceee57e6c4260ccfef
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-16 22:41:16 -0300

    Create class-Model Companies

--- transactions/models.py
+++ transactions/models.py
@@ -2,3 +2,29 @@
 from django.db import models
 from monetary.models import Accounts
 
+
+class Companies(models.Model):
+    """
+        Save companies names
+    Attributes:
+        name:
+
+    """
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+    class Meta:  # pylint: disable=R0903
+        """ Sorted by name field """
+        db_table = "companies"
+        ordering = ["name",]
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.upper()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """ String for representing the ModelName object """
+        return str(self.name)
+
+    objects = models.Manager()
