066c3c782c71c36c29c572a81827fd58271667d5
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-17 01:58:29 -0300

    Tansactions application registered in admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from transactions import models
 
+admin.site.register(models.Companies)
+admin.site.register(models.Categories)
+admin.site.register(models.Registries)
+admin.site.register(models.RegistryItens)
+admin.site.register(models.Payments)
+admin.site.register(models.Methods)
