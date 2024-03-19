f749ad7590f3ce9b26a59d7605d7f94d686b5bf2
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-19 10:43:21 -0300

    Created function create_registries_context_period

--- transactions/facade.py
+++ transactions/facade.py
@@ -2,3 +2,20 @@
 
 from transactions import models as md
 from website import facade as website_facade
+
+
+def create_registries_context_period(month_year):
+    """
+        Creates a context with registries by period.
+    Args:
+        month_year: Month and year used as the basis of the period.
+
+    Returns:
+        Context registries queryset
+
+    """
+    first_day, last_day = website_facade.start_end_dates(month_year)
+    registries = md.Registries.objects.filter(
+        date__range=[first_day, last_day]
+    ).order_by("date")
+    return {"registries": registries}
