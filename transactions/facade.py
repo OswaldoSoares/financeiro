5ea91eef1af40be203c9f458471d1f6bb4f7132e
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-19 10:46:25 -0300

    Created function create_registries_context_period_payd_methods_unique

--- transactions/facade.py
+++ transactions/facade.py
@@ -53,3 +53,29 @@ def create_registries_context_period_paid_methods(month_year):
         payment__date__range=[first_day, last_day]
     )
     return {"methods": methods}
+
+
+def create_registries_context_period_paid_methods_unique(month_year):
+    """
+        Creates a context with payment methods for records by period.
+        Removing equal payments.
+    Args:
+        month_year: Month and year used as the basis of the period.
+
+    Returns:
+        Context methods queryset
+
+    """
+    first_day, last_day = website_facade.start_end_dates(month_year)
+    methods = list(
+        md.Methods.objects.filter(
+            payment__date__range=[first_day, last_day]
+        ).values()
+    )
+    payments_distinct = list(
+        {v["payment_id"]: v["id"] for v in methods}.values()
+    )
+    methods = md.Methods.objects.filter(id__in=payments_distinct).order_by(
+        "payment__registry__date", "payment__registry__ordering"
+    )
+    return {"methods": methods}
