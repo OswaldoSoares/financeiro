a3b653ba40dbf4e1c5c9b9e773825326032ebec1
OswaldoSoares <oswaldosoares@gmail.com>
2024-03-19 10:44:55 -0300

    Created function create_registries_context_period_payd

--- transactions/facade.py
+++ transactions/facade.py
@@ -19,3 +19,20 @@ def create_registries_context_period(month_year):
         date__range=[first_day, last_day]
     ).order_by("date")
     return {"registries": registries}
+
+
+def create_registries_context_period_paid(month_year):
+    """
+        Creates a context with registries payments by period.
+    Args:
+        month_year: Month and year used as the basis of the period.
+
+    Returns:
+        Context payments queryset
+
+    """
+    first_day, last_day = website_facade.start_end_dates(month_year)
+    payments = md.Payments.objects.filter(
+        date__range=[first_day, last_day]
+    ).order_by("date")
+    return {"payments": payments}
