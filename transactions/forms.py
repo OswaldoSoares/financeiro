2cceea79858e1f0d6b89ddf8b6b802468fcb55e5
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-01 10:30:38 -0300

    Class RegistryItensForm created

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,3 +1,17 @@
 import datetime
 from django import forms
 from transactions import models
+class RegistryItensForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = models.RegistryItens
+        fields = "__all__"
+        widgets = {
+            "description": forms.TextInput(attrs={"class": "form-control"}),
+            "brand": forms.TextInput(attrs={"class": "form-control"}),
+            "amount": forms.NumberInput(attrs={"class": "form-control"}),
+            "unitary": forms.NumberInput(attrs={"class": "form-control"}),
+            "favored": forms.TextInput(attrs={"class": "form-control"}),
+        }
