e681262581c3df18d7fd9db4ae551231977256ca
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-01 11:24:23 -0300

    Function js-mostra-itens click created

--- website/static/website/js/transactions.js
+++ website/static/website/js/transactions.js
@@ -5,3 +5,18 @@ $(document).ready(function() {
     $(".card-category-n3-in").hide()
     $(".card-category-n3-out").hide()
 });
+
+$(document).on('click', '.js-mostra-itens', function() {
+    var registry_id = $(this).data("registry-id")
+    $.ajax({
+        type: "GET",
+        url: "view_registry_itens",
+        data : {
+            registry_id: registry_id
+        },
+        success: function(data) {
+            $('.card-registry-itens').html(data["html_registry_itens"]);
+            $('.card-registry-itens').show();
+        },
+    });   
+});
