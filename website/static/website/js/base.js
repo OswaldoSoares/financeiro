564dc4274636c2b94701973a4534e9f08771970c
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-01 11:21:16 -0300

    Function formAjaxSubmit created

--- website/static/website/js/base.js
+++ website/static/website/js/base.js
@@ -33,3 +33,57 @@ function initModalDialog(event, modal_element) {
     modal.data('target', target);
     return modal;
 }
+
+function formAjaxSubmit(modal, action, cbAfterLoad, cbAfterSuccess) {
+    var form = modal.find('.modal-body form');
+    var header = $(modal).find('.modal-header');
+    var btn_save = modal.find('.modal-footer .btn-save');
+    if (btn_save) {
+        modal.find('.modal-body form .form-submit-row').hide();
+        btn_save.off().on('click', function() {
+            modal.find('.modal-body form').submit();
+        });
+    }
+    if (cbAfterLoad) { cbAfterLoad(modal); }
+    modal.find('form input:visible').first().focus();
+    $(form).on('submit', function(event) {
+        event.preventDefault();
+        header.addClass('loading');
+        var url = $(this).attr('action') || action;
+        var formData = new FormData($('.rows').get(0));  
+        $.ajax({
+            type: $(this).attr('method'),
+            url: url,
+            formData: formData,
+            data: $(this).serialize(),
+            beforeSend: function() {
+                $(".box-loader").show()
+                $(".card-registries-unpaid").hide()
+            },
+            success: function(xhr) {
+                $(modal).find('.modal-body').html(xhr['html_form']);
+                if ($(xhr['html_form']).find('.errorlist').length > 0) {
+                    formAjaxSubmit(modal, url, cbAfterLoad, cbAfterSuccess);
+                } else {
+                    $(modal).modal('hide');
+                    if (xhr["html_registries_unpaid"]) {
+                        $(".card-registries-unpaid").html(xhr["html_registries_unpaid"])
+                        $(".card-registries-unpaid").show()
+                    }
+                    if (xhr["html_registry_itens"]) {
+                        $(".card-registries-unpaid").show()
+                        $(".card-registry-itens").html(xhr["html_registry_itens"])
+                        $(".card-registry-itens").show()
+                    }
+                    if (cbAfterSuccess) { 
+                        cbAfterSuccess(modal);
+                    }
+                }
+                $('.box-loader').hide()
+            },
+            complete: function() {
+                header.removeClass('loading');
+            }
+        });
+    });
+}
