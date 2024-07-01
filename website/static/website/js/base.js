ba013441d9652697d5f91073058f424f900390d5
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-01 11:20:35 -0300

    Function initModalDialog created

--- website/static/website/js/base.js
+++ website/static/website/js/base.js
@@ -15,3 +15,21 @@ function openMyModal(event) {
         formAjaxSubmit(modal, url, null, null);
     })
 }
+
+function initModalDialog(event, modal_element) {
+    var modal = $(modal_element);
+    var target = $(event.target);
+    var title = target.data('title') || '';
+    var subtitle = target.data('subtitle') || '';
+    var dialog_class = (target.data('dialog-class') || '') + ' modal-dialog';
+    var icon_class = (target.data('icon') || 'fa-laptop') + ' fa modal-icon';
+    var button_save_label = target.data('button-save-label') || 'Save changes';
+    modal.find('.modal-dialog').attr('class', dialog_class);
+    modal.find('.modal-title').text(title);
+    modal.find('.modal-subtitle').text(subtitle);
+    modal.find('.modal-header .title-wrapper i').attr('class', icon_class);
+    modal.find('.modal-footer .btn-save').text(button_save_label);
+    modal.find('.modal-body').html('');
+    modal.data('target', target);
+    return modal;
+}
