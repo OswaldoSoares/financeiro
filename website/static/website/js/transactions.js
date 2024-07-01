de699027eafc02476ec69f06d891271ea0cca292
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-01 11:23:21 -0300

    Function ready document created

---
+++ website/static/website/js/transactions.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".card-category-n1").hide()
+    $(".card-category-n2-in").hide()
+    $(".card-category-n2-out").hide()
+    $(".card-category-n3-in").hide()
+    $(".card-category-n3-out").hide()
+});
