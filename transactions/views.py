4e048b71b72166ff8215699780992b12699208e2
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-01 10:51:57 -0300

    Function add_registry_itens created

--- transactions/views.py
+++ transactions/views.py
@@ -35,3 +35,17 @@ def add_registry(request):
         context = tf.create_registries_context_unpayd()
         data = tf.create_registries_data_unpaid(request, context)
     return data
+
+
+def add_registry_itens(request):
+    if request.method == "GET":
+        print(request.GET)
+        data = tf.form_registry_itens(request)
+    else:
+        tf.save_reguistry_item(request)
+        context = tf.create_registry_itens_context(
+            request.POST.get("registry_id")
+        )
+        data = tf.create_registry_itens_data(request, context)
+        print(request.POST)
+    return data
