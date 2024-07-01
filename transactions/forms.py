c0a568f22416f1887fe6fede59dea43b643da6f7
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-01 10:31:20 -0300

    Class RegistriesForm created

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,6 +1,53 @@
 import datetime
 from django import forms
 from transactions import models
+class RegistriesForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["date"].initial = datetime.date.today()
+        self.fields["value"].initial = "0.00"
+
+    class Meta:
+        companies = models.Companies.objects.all()
+        categories = models.Categories.objects.all()
+        model = models.Registries
+        fields = "__all__"
+        widgets = {
+            "date": DateInput(
+                format="%Y-%m-%d", attrs={"class": "form-control text-center"}
+            ),
+            "companies": forms.Select(
+                attrs={"class": "form-control"},
+                choices=[(item.id, item.name) for item in companies],
+            ),
+            "category_n1": forms.Select(
+                attrs={"class": "form-control"},
+                choices=[(item.id, item.description) for item in categories],
+            ),
+            "category_n2": forms.Select(
+                attrs={"class": "form-control"},
+                choices=[(item.id, item.description) for item in categories],
+            ),
+            "category_n3": forms.Select(
+                attrs={"class": "form-control"},
+                choices=[(item.id, item.description) for item in categories],
+            ),
+            "value": forms.NumberInput(
+                attrs={"class": "form-control text-right"}
+            ),
+            "in_out": forms.Select(
+                attrs={"class": "form-control text-center"},
+                choices=[("DÉBITO", "DÉBITO"), ("CRÉDITO", "CRÉDITO")],
+            ),
+            "ordering": forms.NumberInput(
+                attrs={"class": "form-control text-right"}
+            ),
+            "obs": forms.Textarea(
+                attrs={"class": "form-control textarea", "rows": "3"}
+            ),
+        }
+
+
 class RegistryItensForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
