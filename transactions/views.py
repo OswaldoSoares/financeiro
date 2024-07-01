61fa821caedb17cf68436e1b4812dfdb7fd26294
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-01 10:51:37 -0300

    Function add_registry created

--- transactions/views.py
+++ transactions/views.py
@@ -25,3 +25,13 @@ def index_transactions(request):
     )
     context.update(tf.create_registries_context_unpayd())
     return render(request, "transactions/index_transactions.html", context)
+
+
+def add_registry(request):
+    if request.method == "GET":
+        data = tf.form_regsitries(request)
+    else:
+        tf.save_registry(request)
+        context = tf.create_registries_context_unpayd()
+        data = tf.create_registries_data_unpaid(request, context)
+    return data
