2c1d7b89a281fc7aca58ce5ec2ca71e1e825706b
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-03 12:38:20 -0300

    Function save_transfer added

--- monetary/facade.py
+++ monetary/facade.py
@@ -12,3 +12,21 @@ def form_transfers(request):
         "monetary/modal_form_transfer.html", context, request=request
     )
     return JsonResponse(data)
+
+
+def save_transfer(request):
+    record = []
+    out_account = request.POST.get("out_account")
+    in_account = request.POST.get("in_account")
+    input_value = float(request.POST.get("value"))
+    record.append(
+        md.Transfers(
+            out_account_id=out_account,
+            in_account_id=in_account,
+            value=input_value,
+            date=request.POST.get("date"),
+        )
+    )
+    md.Transfers.objects.bulk_create(record)
+    save_new_balance(out_account, input_value, "minus")
+    save_new_balance(in_account, input_value, "plus")
