53f6313fbc4e4c11e9478575e391a7626e5cb4a9
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-03 12:42:02 -0300

    Class TransferForm created

--- monetary/forms.py
+++ monetary/forms.py
@@ -5,3 +5,28 @@ from monetary import models
 
 class DateInput(forms.DateInput):
     input_type = "date"
+
+
+class TransferForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["date"].initial = datetime.date.today()
+
+    class Meta:
+        accounts = models.Accounts.objects.all()
+        model = models.Transfers
+        fields = "__all__"
+        widgets = {
+            "out_account": forms.Select(
+                attrs={"class": "form-control"},
+                choices=[(item.id, item.name) for item in accounts],
+            ),
+            "in_account": forms.Select(
+                attrs={"class": "form-control"},
+                choices=[(item.id, item.name) for item in accounts],
+            ),
+            "value": forms.NumberInput(attrs={"class": "form-control"}),
+            "date": DateInput(
+                format="%Y-%m-%d", attrs={"class": "form-control text-center"}
+            ),
+        }
