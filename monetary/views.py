8715c16b9f4f60720d9cd2977640d553b178e896
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-03 12:48:25 -0300

    Function index_monetary added

--- monetary/views.py
+++ monetary/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from monetary import facade as mf
 
 # Create your views here.
+
+
+def index_monetary(request):
+    context = {"nada": "nada"}
+    return render(request, "monetary/index_monetary.html", context)
