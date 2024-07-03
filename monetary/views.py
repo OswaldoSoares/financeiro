6a9a00f6a82b71d33d6c978c8a601c4951aacd3f
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-03 12:49:20 -0300

    Function add_transfer added

--- monetary/views.py
+++ monetary/views.py
@@ -7,3 +7,12 @@ from monetary import facade as mf
 def index_monetary(request):
     context = {"nada": "nada"}
     return render(request, "monetary/index_monetary.html", context)
+
+
+def add_transfer(request):
+    if request.method == "GET":
+        data = mf.form_transfers(request)
+    else:
+        mf.save_transfer(request)
+        data = mf.create_transfer_data_message(request)
+    return data
