3cab61b6518b7bfefc14dc02349a87155257d2ac
OswaldoSoares <oswaldosoares@gmail.com>
2024-07-03 12:37:42 -0300

    Function form_transfers added

--- monetary/facade.py
+++ monetary/facade.py
@@ -2,3 +2,13 @@ from django.template.loader import render_to_string
 from django.http import JsonResponse
 from monetary import forms as fr
 from monetary import models as md
+
+
+def form_transfers(request):
+    form = fr.TransferForm()
+    context = {"form": form}
+    data = {}
+    data["html_modal"] = render_to_string(
+        "monetary/modal_form_transfer.html", context, request=request
+    )
+    return JsonResponse(data)
